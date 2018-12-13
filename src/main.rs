9a0f9b9ac1adb99edc043b83fdc8bb39a7628417
Keith Amling <keith.amling@gmail.com>
2018-12-13 13:24:13 -0800

    checkpt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(mpsc_select)]
+
 use std::process::Command;
 use std::process::Stdio;
 use std::select;
@@ -8,6 +10,13 @@ fn main() {
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn();
+
+    let (stdin_tx, stdin_rx) = mpsc::channel()
+    let (stdout_tx, stdout_rx) = mpsc::channel()
+
+    thread::spawn(move|| {
+        ...
+    });
 
     select! {
     }
