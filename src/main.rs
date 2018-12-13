af5bb5d19e4669beff8af459a8876fdd77256a0b
Keith Amling <keith.amling@gmail.com>
2018-12-13 13:09:02 -0800

    uh, oh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+use std::process::Stdio;
+use std::select;
+
 fn main() {
     println!("Hello, world!");
+    let p = Command::new("cat")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn();
+
+    select! {
+    }
 }
