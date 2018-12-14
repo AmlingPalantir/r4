aa8da87b5df9d61f1e116fe5052299c208969304
Keith Amling <keith.amling@gmail.com>
2018-12-14 11:51:16 -0800

    maybe

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-#![feature(mpsc_select)]
-
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::LineWriter;
+use std::io::Write;
 use std::process::Command;
 use std::process::Stdio;
 use std::sync::mpsc;
 use std::thread;
+
+enum ChannelElement {
+    Line(String),
+    AllowInput,
+    End,
+}
 
 fn main() {
     println!("Hello, world!");
@@ -15,27 +21,52 @@ fn main() {
         .spawn()
         .unwrap();
 
-    //let (stdin_tx, stdin_rx) = mpsc::channel();
+    let (stdin_tx, stdin_rx) = mpsc::channel();
     let (stdout_tx, stdout_rx) = mpsc::channel();
 
-    thread::spawn(move|| {
-        let r = BufReader::new(p.stdout.unwrap());
-        for line in r.lines() {
-            stdout_tx.send(Some(line)).unwrap();
-        }
-        stdout_tx.send(None).unwrap();
-    });
+    let mut stdin_space = 1024;
 
-    std::select! {
-        e = stdout_rx.recv() => {
-            match(e) {
+    let p_stdin = p.stdin;
+    let stdout_tx_1 = stdout_tx.clone();
+    thread::spawn(move|| {
+        let mut r = LineWriter::new(p_stdin.unwrap());
+        'E: loop {
+            let e = stdin_rx.recv().unwrap();
+            match e {
                 Some(line) => {
-                    println!("Line: {}", line)
+                    r.write_all(line).unwrap();
+                    r.write_all(b"\n").unwrap();
+                    stdout_tx_1.send(ChannelElement::AllowInput).unwrap()
                 }
                 None => {
-                    println!("EOF")
+                    break 'E;
                 }
-            }
+            };
         }
+    });
+
+    let p_stdout = p.stdout;
+    let stdout_tx_2 = stdout_tx.clone();
+    thread::spawn(move|| {
+        let r = BufReader::new(p_stdout.unwrap());
+        for line in r.lines() {
+            stdout_tx_2.send(ChannelElement::Line(line.unwrap())).unwrap();
+        }
+        stdout_tx_2.send(ChannelElement::End).unwrap();
+    });
+
+    loop {
+        let e = stdout_rx.recv().unwrap();
+        match e {
+            ChannelElement::Line(line) => {
+                println!("Line: {}", line)
+            }
+            ChannelElement::AllowInput => {
+                stdin_space += 1;
+            }
+            ChannelElement::End => {
+                println!("EOF")
+            }
+        };
     }
 }
