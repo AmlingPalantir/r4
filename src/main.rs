525fbea65ae57f62aadc70e38c1da8cf594251ed
Keith Amling <keith.amling@gmail.com>
2018-12-14 12:33:03 -0800

    maybe

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,19 @@ use std::io::Write;
 use std::io;
 use std::process::Command;
 use std::process::Stdio;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 use std::sync::mpsc;
 use std::thread;
 
 trait Stream {
-    fn write_line(&self, String);
-    fn close(&self);
-}
-
-enum ChannelElement {
-    Line(String),
-    AllowInput,
-    End,
+    fn write_line(&mut self, String);
+    fn close(&mut self);
 }
 
 fn main() {
-    let mut os = StdoutStream::new();
-    let mut os = ProcessStream::new(&mut os, &["cat"]);
+    let os = StdoutStream::new();
+    let mut os = ProcessStream::new(Box::new(os), &["cat"]);
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
@@ -42,20 +38,29 @@ impl StdoutStream {
 }
 
 impl Stream for StdoutStream {
-    fn write_line(&self, line: String) {
+    fn write_line(&mut self, line: String) {
         println!("StdoutStream line: {}", line);
     }
 
-    fn close(&self) {
+    fn close(&mut self) {
     }
 }
 
+enum ChannelElement {
+    Line(String),
+    AllowInput,
+    End,
+}
+
 struct ProcessStream {
+    os: Box<Stream>,
     stdin_space: u8,
+    stdin_tx: Sender<Option<String>>,
+    stdout_rx: Receiver<ChannelElement>,
 }
 
 impl ProcessStream {
-    fn new(os: &mut Stream, args: &[&str]) -> ProcessStream
+    fn new(os: Box<Stream>, args: &[&str]) -> ProcessStream
     {
         let p = Command::new(args[0])
             .args(&args[1..])
@@ -97,34 +102,38 @@ impl ProcessStream {
         });
 
         return ProcessStream {
+            os: os,
             stdin_space: 1,
+            stdin_tx: stdin_tx,
+            stdout_rx: stdout_rx,
         };
     }
 }
 
 impl Stream for ProcessStream {
-    fn write_line(&self, line: String) {
-//        while stdin_space == 0 {
-//            let e = stdout_rx.recv().unwrap();
-//            match e {
-//                ChannelElement::Line(line) => {
-//                    println!("[line ferry] Output line: {}", line);
-//                }
-//                ChannelElement::AllowInput => {
-//                    println!("[line ferry] AllowInput");
-//                    stdin_space += 1;
-//                }
-//                ChannelElement::End => {
-//                    println!("[line ferry] EOF");
-//                }
-//            };
-//        }
-//
-//        stdin_space -= 1;
-//        stdin_tx.send(Some(line)).unwrap();
+    fn write_line(&mut self, line: String) {
+        while self.stdin_space == 0 {
+            let e = self.stdout_rx.recv().unwrap();
+            match e {
+                ChannelElement::Line(line) => {
+                    println!("[line ferry] Output line: {}", line);
+                    self.os.write_line(line)
+                }
+                ChannelElement::AllowInput => {
+                    println!("[line ferry] AllowInput");
+                    self.stdin_space += 1;
+                }
+                ChannelElement::End => {
+                    println!("[line ferry] EOF");
+                }
+            };
+        }
+
+        self.stdin_space -= 1;
+        self.stdin_tx.send(Some(line)).unwrap();
     }
 
-    fn close(&self) {
+    fn close(&mut self) {
 //    stdin_tx.send(None).unwrap();
 //    'E: loop {
 //        let e = stdout_rx.recv().unwrap();
