b384888ff6e250443022f9eb01af4ceac5d7a5af
Keith Amling <keith.amling@gmail.com>
2018-12-20 15:14:06 -0800

    much better

--- operation/src/test.rs
+++ operation/src/test.rs
@@ -1,5 +1,3 @@
-use stream::Stream;
-
 use ClosureStreamWrapper;
 use Operation;
 use StreamWrapper;
@@ -8,28 +6,6 @@ use record::Record;
 use std::collections::VecDeque;
 use std::sync::Arc;
 use stream::Entry;
-
-struct MyOperationStream {
-    os: Box<Stream>,
-    msg: Arc<str>,
-    n: u32,
-}
-
-impl Stream for MyOperationStream {
-    fn write(&mut self, e: Entry) -> bool {
-        let mut r = e.to_record();
-
-        self.n += 1;
-        r.set_path("n", Record::from_primitive(self.n));
-        r.set_path("msg", Record::from_primitive_string(self.msg.clone()));
-
-        return self.os.write(Entry::Record(r));
-    }
-
-    fn close(&mut self) {
-        self.os.close();
-    }
-}
 
 pub(crate) fn names() -> Vec<&'static str> {
     return vec!["test"];
@@ -44,10 +20,17 @@ impl Operation for Impl {
         let msg: Arc<str> = Arc::from(args.pop_front().unwrap());
 
         return ClosureStreamWrapper::new(move |os| {
-            return Box::new(MyOperationStream {
-                os: os,
-                msg: msg.clone(),
-                n: 0,
+            let mut n = 0;
+            let msg = msg.clone();
+
+            return stream::transform(os, move |e| {
+                let mut r = e.to_record();
+
+                n += 1;
+                r.set_path("n", Record::from_primitive(n));
+                r.set_path("msg", Record::from_primitive_string(msg.clone()));
+
+                return Entry::Record(r);
             });
         });
     }
