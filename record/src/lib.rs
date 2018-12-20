3aa78bdb69241b5e73f33836fab6b5df80a6462e
Keith Amling <keith.amling@gmail.com>
2018-12-19 17:52:39 -0800

    resurrect array

--- record/src/lib.rs
+++ record/src/lib.rs
@@ -9,6 +9,7 @@ use std::vec::Vec;
 enum JsonPart {
     Primitive(JsonPrimitive),
     // TODO: less crummy Vec (better splice?)
+    Array(Vec<Arc<JsonPart>>),
     Hash(BTreeMap<Arc<str>, Arc<JsonPart>>),
 }
 
@@ -31,13 +32,28 @@ impl Record {
         panic!();
     }
 
+    fn get_array(&self, key: usize) -> Option<Record> {
+        if let JsonPart::Array(ref arr) = *self.0 {
+            return match arr.get(key) {
+                Some(arc) => Some(Record(arc.clone())),
+                None => None,
+            };
+        }
+        panic!();
+    }
+
     fn get_path(&self, path: Arc<str>) -> Record {
         let mut ret = self.clone();
         let null = Record(Arc::new(JsonPart::Primitive(JsonPrimitive::Null)));
 
         for part in path.split('/') {
             let next;
-            next = ret.get_hash(Arc::from(part));
+            if part.starts_with('#') {
+                next = ret.get_array(part[1..].parse().unwrap())
+            }
+            else {
+                next = ret.get_hash(Arc::from(part));
+            }
             match next {
                 Some(next) => {
                     ret = next;
@@ -62,6 +78,29 @@ impl Record {
             panic!();
         }
 
-        return path.split('/').fold(Arc::make_mut(&mut self.0), |r, part| _get_hash_mut(r, Arc::from(part)));
+        fn _get_array_mut(r: &mut JsonPart, key: usize) -> &mut JsonPart {
+            if let JsonPart::Primitive(JsonPrimitive::Null) = *r {
+                *r = JsonPart::Array(Vec::new());
+            }
+            if let JsonPart::Array(ref mut arr) = *r {
+                if key < 0 {
+                    panic!();
+                }
+                while key >= arr.len() {
+                    arr.push(Arc::new(JsonPart::Primitive(JsonPrimitive::Null)));
+                }
+                return &mut arr[key];
+            }
+            panic!();
+        }
+
+        return path.split('/').fold(Arc::make_mut(&mut self.0), |r, part| {
+            if part.starts_with('#') {
+                return _get_array_mut(r, part[1..].parse().unwrap());
+            }
+            else {
+                return _get_hash_mut(r, Arc::from(part));
+            }
+        });
     }
 }
