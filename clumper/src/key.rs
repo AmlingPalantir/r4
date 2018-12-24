20b63e669aba1284b5955189e025fcd95716327e
Keith Amling <keith.amling@gmail.com>
2018-12-24 10:27:15 -0800

    So much better.

--- clumper/src/key.rs
+++ clumper/src/key.rs
@@ -5,8 +5,6 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use stream::Entry;
 use stream::Stream;
-use stream::StreamTrait;
-
 pub(crate) fn names() -> Vec<&'static str> {
     return vec!["k", "key"];
 }
@@ -19,52 +17,39 @@ impl ClumperBe for Impl {
     type Args = OneStringArgs;
 
     fn stream(k: &Arc<str>, bsw: Box<Fn(Vec<(Arc<str>, Record)>) -> Stream>) -> Stream {
-        let s = Stream::new(KeyStream {
-            k: k.clone(),
-            bsw: bsw,
-            substreams: HashMap::new(),
-        });
-        return Stream::compound(Stream::parse(), s);
-    }
-}
+        let k = k.clone();
 
-struct KeyStream {
-    k: Arc<str>,
-    bsw: Box<Fn(Vec<(Arc<str>, Record)>) -> Stream>,
-    substreams: HashMap<Record, Stream>,
-}
+        return Stream::compound(
+            Stream::parse(),
+            Stream::closures(
+                HashMap::new(),
+                move |s, e, w| {
+                    match e {
+                        Entry::Bof(_file) => {
+                        },
+                        Entry::Record(r) => {
+                            let v = r.get_path(&k);
 
-impl KeyStream {
-    fn find_stream(&mut self, v: Record) -> &mut Stream {
-        let k = &self.k;
-        let bsw = &self.bsw;
-        return self.substreams.entry(v.clone()).or_insert_with(|| {
-            return bsw(vec![(k.clone(), v)]);
-        });
-    }
-}
+                            let substream = s.entry(v.clone()).or_insert_with(|| {
+                                return bsw(vec![(k.clone(), v)]);
+                            });
 
-impl StreamTrait for KeyStream {
-    fn write(&mut self, e: Entry, w: &mut FnMut(Entry) -> bool) -> bool {
-        match e {
-            Entry::Bof(_file) => {
-            },
-            Entry::Record(r) => {
-                let v = r.get_path(&self.k);
-                self.find_stream(v).write(Entry::Record(r), w);
-            },
-            Entry::Line(_line) => {
-                panic!("Unexpected line in KeyStream");
-            },
-        }
-        // Sad, but you could always be opening a new stream so we can never be
-        // sure we're done.
-        return true;
-    }
-
-    fn close(self: Box<KeyStream>, w: &mut FnMut(Entry) -> bool) {
-        for (_, substream) in self.substreams.into_iter() {
-            substream.close(w);
-        }
+                            substream.write(Entry::Record(r), w);
+                        },
+                        Entry::Line(_line) => {
+                            panic!("Unexpected line in KeyStream");
+                        },
+                    }
+                    // Sad, but you could always be opening a new stream so we can never be
+                    // sure we're done.
+                    return true;
+                },
+                |s, w| {
+                    for (_, substream) in s.into_iter() {
+                        substream.close(w);
+                    }
+                },
+            ),
+        );
     }
 }
