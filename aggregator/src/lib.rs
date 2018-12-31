73e81ab33ef2b03e58fb5b7d3e49364d9e3adf25
Keith Amling <amling@palantir.com>
2018-12-31 14:06:16 -0800

    Keep sum as i64 where possible.

--- aggregator/src/lib.rs
+++ aggregator/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate misc;
 extern crate record;
 #[macro_use]
 extern crate registry;
--- aggregator/src/sum.rs
+++ aggregator/src/sum.rs
@@ -1,3 +1,4 @@
+use misc::Either;
 use record::Record;
 use record::RecordTrait;
 use registry::args::OneStringArgs;
@@ -9,20 +10,41 @@ pub(crate) type Impl = AggregatorRegistrant<ImplBe>;
 
 pub(crate) struct ImplBe;
 
+#[derive(Clone)]
+pub(crate) struct State(Either<i64, f64>);
+
+impl Default for State {
+    fn default() -> Self {
+        return State(Either::Left(0));
+    }
+}
+
 impl AggregatorBe for ImplBe {
     type Args = OneStringArgs;
-    type State = f64;
+    type State = State;
 
     fn names() -> Vec<&'static str> {
         return vec!["sum"];
     }
 
-    fn add(state: &mut f64, a: &Arc<str>, r: Record) {
-        let v = r.get_path(a);
-        *state += v.coerce_f64();
+    fn add(state: &mut State, a: &Arc<str>, r: Record) {
+        let n1 = r.get_path(a).coerce_num();
+        let n2 = state.0.clone();
+
+        if let Either::Left(i1) = n1 {
+            if let Either::Left(i2) = n2 {
+                *state = State(Either::Left(i1 + i2));
+                return;
+            }
+        }
+
+        let f1 = n1.map_left(|i| i as f64).join();
+        let f2 = n2.map_left(|i| i as f64).join();
+
+        *state = State(Either::Right(f1 + f2));
     }
 
-    fn finish(state: f64, _a: &Arc<str>) -> Record {
-        return Record::from(state);
+    fn finish(state: State, _a: &Arc<str>) -> Record {
+        return state.0.map_left(Record::from).map_right(Record::from).join();
     }
 }
