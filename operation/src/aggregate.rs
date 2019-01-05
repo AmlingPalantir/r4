776722098a0f7e0fae25a1c7dc8f6708a3c56af0
Keith Amling <keith.amling@gmail.com>
2019-01-04 16:07:32 -0800

    collate --incremental

--- operation/src/aggregate.rs
+++ operation/src/aggregate.rs
@@ -1,5 +1,6 @@
 use aggregator::BoxedAggregator;
 use opts::parser::OptParserView;
+use opts::vals::BooleanOption;
 use opts::vals::UnvalidatedOption;
 use record::Record;
 use record::RecordTrait;
@@ -15,6 +16,7 @@ use validates::Validates;
 #[derive(Validates)]
 pub struct Options {
     aggs: UnvalidatedOption<Vec<(String, BoxedAggregator)>>,
+    incremental: BooleanOption,
 }
 
 pub(crate) type Impl = OperationRegistrant<ImplBe>;
@@ -33,34 +35,55 @@ impl OperationBe2 for ImplBe2 {
     fn options<'a>(opt: &mut OptParserView<'a, Options>) {
         aggregator::REGISTRY.labelled_single_options(&mut opt.sub(|p| &mut p.aggs.0), &["a", "agg", "aggregator"]);
         aggregator::REGISTRY.labelled_multiple_options(&mut opt.sub(|p| &mut p.aggs.0), &["a", "agg", "aggregator"]);
+        opt.sub(|p| &mut p.incremental).match_zero(&["incremental"], BooleanOption::set);
+        opt.sub(|p| &mut p.incremental).match_zero(&["no-incremental"], BooleanOption::clear);
     }
 
     fn stream(o: Arc<OptionsValidated>) -> Stream {
+        struct State {
+            o: Arc<OptionsValidated>,
+            aggs: Vec<(String, BoxedAggregator)>,
+        }
+        fn aggregate_record(aggs: Vec<(String, BoxedAggregator)>) -> Record {
+            let mut rhs = Record::empty_hash();
+            for (label, state) in aggs.clone().into_iter() {
+                rhs.set_path(&label, state.finish());
+            }
+            return rhs;
+        }
+
         return stream::compound(
             stream::parse(),
             stream::closures(
-                o.aggs.clone(),
-                |s, e, _w| {
+                State {
+                    o: o.clone(),
+                    aggs: o.aggs.clone(),
+                },
+                |s, e, w| {
                     match e {
                         Entry::Bof(_file) => {
+                            return true;
                         }
                         Entry::Record(r) => {
-                            for (_, ref mut state) in s.iter_mut() {
+                            for (_, ref mut state) in s.aggs.iter_mut() {
                                 state.add(r.clone());
                             }
+
+                            if s.o.incremental {
+                                return w(Entry::Record(aggregate_record(s.aggs.clone())));
+                            }
+                            return true;
                         }
                         Entry::Line(_line) => {
                             panic!("Unexpected line in AggregateStream");
                         }
                     }
-                    return true;
                 },
                 |s, w| {
-                    let mut r = Record::empty_hash();
-                    for (label, state) in s.into_iter() {
-                        r.set_path(&label, state.finish());
+                    if !s.o.incremental {
+                        w(Entry::Record(aggregate_record(s.aggs)));
+                        return;
                     }
-                    w(Entry::Record(r));
                 },
             ),
         );
