fd13e88a17445672c0d3fb6214b1456cc1729844
Keith Amling <amling@palantir.com>
2019-01-17 17:56:15 -0800

    Start OptionsPileElement API

--- opts/src/parser.rs
+++ opts/src/parser.rs
@@ -14,9 +14,47 @@ enum ExtraHandler<P> {
     Hard(CbMany<P>),
 }
 
-enum OptionsPileElement<P> {
+pub struct OptionsPileElement<P> {
+    m: OptionsPileElementMatch<P>,
+}
+
+enum OptionsPileElementMatch<P> {
     Args(Vec<String>, usize, CbMany<P>),
     Extra(ExtraHandler<P>),
+}
+
+impl<P> OptionsPileElement<P> {
+    pub fn match_single<F: Fn(&mut P, &str) -> ValidationResult<()> + 'static>(aliases: &[&str], f: F) -> OptionsPileElement<P> {
+        return Self::match_n(aliases, 1, move |p, a| f(p, &a[0]));
+    }
+
+    pub fn match_zero<F: Fn(&mut P) -> ValidationResult<()> + 'static>(aliases: &[&str], f: F) -> OptionsPileElement<P> {
+        return Self::match_n(aliases, 0, move |p, _a| f(p));
+    }
+
+    pub fn match_n<F: Fn(&mut P, &[String]) -> ValidationResult<()> + 'static>(aliases: &[&str], argct: usize, f: F) -> OptionsPileElement<P> {
+        return OptionsPileElement {
+            m: OptionsPileElementMatch::Args(aliases.iter().map(|s| s.to_string()).collect(), argct, PointerRc(Rc::new(f))),
+        };
+    }
+
+    pub fn match_extra_soft<F: Fn(&mut P, &str) -> ValidationResult<bool> + 'static>(f: F) -> OptionsPileElement<P> {
+        return OptionsPileElement {
+            m: OptionsPileElementMatch::Extra(ExtraHandler::Soft(PointerRc(Rc::new(f)))),
+        };
+    }
+
+    pub fn match_extra_hard<F: Fn(&mut P, &[String]) -> ValidationResult<()> + 'static>(f: F) -> OptionsPileElement<P> {
+        return OptionsPileElement {
+            m: OptionsPileElementMatch::Extra(ExtraHandler::Hard(PointerRc(Rc::new(f)))),
+        };
+    }
+
+    fn map_match<P2, F: FnOnce(OptionsPileElementMatch<P>) -> OptionsPileElementMatch<P2>>(self, f: F) -> OptionsPileElement<P2> {
+        return OptionsPileElement {
+            m: f(self.m),
+        };
+    }
 }
 
 pub struct OptionsPile<P>(Vec<OptionsPileElement<P>>);
@@ -27,23 +65,23 @@ impl<P: 'static> OptionsPile<P> {
     }
 
     pub fn match_single<F: Fn(&mut P, &str) -> ValidationResult<()> + 'static>(&mut self, aliases: &[&str], f: F) {
-        self.match_n(aliases, 1, move |p, a| f(p, &a[0]));
+        self.0.push(OptionsPileElement::match_single(aliases, f));
     }
 
     pub fn match_zero<F: Fn(&mut P) -> ValidationResult<()> + 'static>(&mut self, aliases: &[&str], f: F) {
-        self.match_n(aliases, 0, move |p, _a| f(p));
+        self.0.push(OptionsPileElement::match_zero(aliases, f));
     }
 
     pub fn match_n<F: Fn(&mut P, &[String]) -> ValidationResult<()> + 'static>(&mut self, aliases: &[&str], argct: usize, f: F) {
-        self.0.push(OptionsPileElement::Args(aliases.iter().map(|s| s.to_string()).collect(), argct, PointerRc(Rc::new(f))));
+        self.0.push(OptionsPileElement::match_n(aliases, argct, f));
     }
 
     pub fn match_extra_soft<F: Fn(&mut P, &str) -> ValidationResult<bool> + 'static>(&mut self, f: F) {
-        self.0.push(OptionsPileElement::Extra(ExtraHandler::Soft(PointerRc(Rc::new(f)))));
+        self.0.push(OptionsPileElement::match_extra_soft(f));
     }
 
     pub fn match_extra_hard<F: Fn(&mut P, &[String]) -> ValidationResult<()> + 'static>(&mut self, f: F) {
-        self.0.push(OptionsPileElement::Extra(ExtraHandler::Hard(PointerRc(Rc::new(f)))));
+        self.0.push(OptionsPileElement::match_extra_hard(f));
     }
 
     pub fn add(&mut self, mut other: OptionsPile<P>) {
@@ -57,16 +95,16 @@ impl<P: 'static> OptionsPile<P> {
     pub fn to_parser(&self) -> OptParser<P> {
         let mut opt = OptParser::default();
         for e in self.0.iter() {
-            match e {
-                OptionsPileElement::Args(aliases, argct, f) => {
+            match e.m {
+                OptionsPileElementMatch::Args(ref aliases, argct, ref f) => {
                     for alias in aliases {
-                        opt.named.insert(&alias, (*argct, f.clone()));
+                        opt.named.insert(&alias, (argct, f.clone()));
                     }
                 }
-                OptionsPileElement::Extra(ExtraHandler::Soft(h)) => {
+                OptionsPileElementMatch::Extra(ExtraHandler::Soft(ref h)) => {
                     opt.extra.push(ExtraHandler::Soft(h.clone()));
                 }
-                OptionsPileElement::Extra(ExtraHandler::Hard(h)) => {
+                OptionsPileElementMatch::Extra(ExtraHandler::Hard(ref h)) => {
                     opt.extra.push(ExtraHandler::Hard(h.clone()));
                 }
             }
@@ -78,11 +116,13 @@ impl<P: 'static> OptionsPile<P> {
         let f1 = Rc::new(f1);
         return OptionsPile(self.0.into_iter().map(|e| {
             let f1 = f1.clone();
-            return match e {
-                OptionsPileElement::Args(aliases, argct, f) => OptionsPileElement::Args(aliases, argct, PointerRc(Rc::new(move |p, a| (f.0)(f1(p), a)))),
-                OptionsPileElement::Extra(ExtraHandler::Soft(h)) => OptionsPileElement::Extra(ExtraHandler::Soft(PointerRc(Rc::new(move |p, a| (h.0)(f1(p), a))))),
-                OptionsPileElement::Extra(ExtraHandler::Hard(h)) => OptionsPileElement::Extra(ExtraHandler::Hard(PointerRc(Rc::new(move |p, a| (h.0)(f1(p), a))))),
-            };
+            return e.map_match(|m| {
+                return match m {
+                    OptionsPileElementMatch::Args(aliases, argct, f) => OptionsPileElementMatch::Args(aliases, argct, PointerRc(Rc::new(move |p, a| (f.0)(f1(p), a)))),
+                    OptionsPileElementMatch::Extra(ExtraHandler::Soft(h)) => OptionsPileElementMatch::Extra(ExtraHandler::Soft(PointerRc(Rc::new(move |p, a| (h.0)(f1(p), a))))),
+                    OptionsPileElementMatch::Extra(ExtraHandler::Hard(h)) => OptionsPileElementMatch::Extra(ExtraHandler::Hard(PointerRc(Rc::new(move |p, a| (h.0)(f1(p), a))))),
+                };
+            });
         }).collect());
     }
 }
